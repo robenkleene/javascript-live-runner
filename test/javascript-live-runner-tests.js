5ac79d90c30701f3062e5bd22d5422a24184d84c
Roben Kleene <robenk@gmail.com>
2016-05-24 20:50:47 -0400

    Setup a basic test

---
+++ test/javascript-live-runner-tests.js
@@ -0,0 +1,8 @@
+var expect = require("chai").expect;
+var liveRunner = require("../dist/javascript-live-runner.js");
+
+describe("javascript-live-runner", function() {
+  it("returns something", function() {
+    expect(2).to.equal(2);
+  });
+});
