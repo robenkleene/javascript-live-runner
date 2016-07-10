5960baab6d74bf97d01ec26024525de0a1ca6d6c
Roben Kleene <robenk@gmail.com>
2016-07-10 18:00:40 -0400

    Add unfinished test for error handling

--- test/javascript-live-runner-tests.js
+++ test/javascript-live-runner-tests.js
@@ -2,13 +2,17 @@ var expect = require("chai").expect;
 var LiveRunner = require("../dist/javascript-live-runner.js");
 
 // TODO: Figure out error handling, if input generates an error, do what?
+// ideally we'd just return the error and half execution. But this is 
+// it's hard to identify and error when just wrapping a REPL, because
+// errors are just output without distinguishing characteristics from regular
+// input.
 
 var liveRunner = null;
 describe("javascript-live-runner", function() {
   beforeEach(function () {
     liveRunner = new LiveRunner();
   });
-  it('performs a simple calculation', function(done) {
+  it('it can perform a simple calculation', function(done) {
     var code = '1 + 1';
     liveRunner.on('output', function(input, output) {
       expect(output).to.equal('2');
@@ -25,13 +29,26 @@ describe("javascript-live-runner", function() {
     });
     liveRunner.read(code);
   });
-  it('It allows printing the prompt', function(done) {
+  it('it allows printing the prompt', function(done) {
     var promptText = "> ";
     var code = 'console.log("' + promptText + '")';
+    var testOutputs = [promptText, 'undefined'];
     liveRunner.on('output', function(input, output) {
-      expect(output).to.equal(promptText);
+      console.log("Output = " + output + "END");
+      var testOutput = testOutputs.splice(0, 1)[0];
+      expect(output).to.equal(testOutput);
       done();
     });
     liveRunner.read(code);
   });
+  it('it calls the callback with an error', function(done) {
+    var code = "asdf";
+    liveRunner.on('output', function(input, output) {
+      console.log('output =' + output);
+      // expect(input).to.equal(code);
+      done();
+    });
+    liveRunner.read(code);
+  });
+  // TODO: "it halts execution when an error is encountered."
 });
